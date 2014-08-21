9a9f086193a037ba572f54e9f70f659f93a404cd
Keegan Mullaney <keegan@kmauthorized.com>
2014-08-21 16:47:28 +0800

    add user option for HTTPS or SSH
    On branch master
    Changes to be committed:
    
    	modified:   mm_init.sh

--- mm_init.sh
+++ mm_init.sh
@@ -32,13 +32,33 @@ RUBY_VERSION='2.1.2'       # to check version - https://www.ruby-lang.org/en/dow
 RUBY_URL="https://get.rvm.io"
 
 # local repository location
-MM_REPOS="$HOME/repos"
+REPOS="$HOME/repos"
 if [ -d $HOME/Dropbox ]; then
-   MM_REPOS=$HOME/Dropbox/Repos
+   REPOS=$HOME/Dropbox/Repos
 fi
+PROJECT_DIRECTORY="$REPOS/$MIDDLEMAN_DOMAIN"
 
 # make repos directory if it doesn't exist
-mkdir -pv $MM_REPOS
+mkdir -pv $REPOS
+
+# files
+SSH_KEY="$HOME/.ssh/id_rsa"
+GIT_IGNORE="$HOME/.gitignore"
+
+# init option variables
+HTTPS=false
+SSH=false
+
+echo
+echo "Do you wish to use HTTPS or SSH for git operations?"
+select yn in "HTTPS" "SSH"; do
+   case $yn in
+      "HTTPS") HTTPS=true;;
+        "SSH") SSH=true;;
+            *) echo "case not found..."
+   esac
+   break
+done
 
 # install Ruby and RubyGems
 read -p "Press enter to install ruby and rubygems..."
@@ -85,12 +105,82 @@ else
    gem install middleman
 fi
 
+# install git
+if rpm -q git; then
+   echo "git was already installed"
+else
+   echo
+   read -p "Press enter to install git..."
+   yum -y install git
+fi
+
+# configure git
+if git config --list | grep -q $GIT_IGNORE; then
+   echo "git was already configured."
+else
+   echo
+   read -p "Press enter to configure git..."
+   # specify a user
+   git config --global user.name "$REAL_NAME"
+   git config --global user.email "$EMAIL_ADDRESS"
+   # select a text editor
+   git config --global core.editor vi
+   # add some SVN-like aliases
+   git config --global alias.st status
+   git config --global alias.co checkout
+   git config --global alias.br branch
+   git config --global alias.up rebase
+   git config --global alias.ci commit
+   # set default push and pull behavior to the old method
+   git config --global push.default matching
+   git config --global pull.default matching
+   # create a global .gitignore file
+   echo -e "# global list of file types to ignore \
+\n \
+\n# gedit temp files \
+\n*~" > $GIT_IGNORE
+   git config --global core.excludesfile $GIT_IGNORE
+   echo "git was configured"
+fi
+
+if $SSH; then
+   echo
+   read -p "Press enter to check if id_rsa exists"
+   if [ -e $SSH_KEY ]; then
+      echo "$SSH_KEY already exists"
+   else
+      # create a new ssh key with provided ssh key comment
+      echo "create new key: $SSH_KEY"
+      read -p "Press enter to generate a new SSH key"
+      ssh-keygen -b 4096 -t rsa -C "$SSH_KEY_COMMENT"
+      echo "SSH key generated"
+      echo
+      echo "***IMPORTANT***"
+      echo "copy contents of id_rsa.pub (printed below) to the SSH keys section"
+      echo " of your GitHub account."
+      echo "highlight the text with your mouse and press ctrl+shift+c to copy"
+      echo
+      cat $SSH_KEY.pub
+      echo
+      read -p "Press enter to continue..."
+   fi
+fi
+
+# linux-deploy-scripts repository
+if $SSH; then
+   echo
+   echo "Have you copied id_rsa.pub (above) to the SSH keys section"
+   echo "of your GitHub account?"
+fi
+echo
+read -p "Press enter when ready..."
+
 # change to repos directory
-cd $MM_REPOS
+cd $REPOS
 echo "changing directory to $_"
 
 # generate a blog template for Middleman
-if [ -d "$MM_REPOS/$MIDDLEMAN_DOMAIN" ]; then
+if [ -d "$PROJECT_DIRECTORY" ]; then
    echo "$MIDDLEMAN_DOMAIN directory already exists, skipping middleman init..."
    read -p "Press enter to update your Middleman site instead..."
    bower update
@@ -133,25 +223,37 @@ else
    echo "middleman-blog added to Gemfile"
 fi
 
-# assign the original repository to a remote called "upstream"
-if git config --list | grep -q $UPSTREAM_REPO; then
-   echo "upstream repo already configured: https://github.com/$UPSTREAM_REPO"
+# config.rb
+
+
+# check if an upstream repo exists
+if echo $UPSTREAM_REPO | grep -q $GITHUB_USER; then
+   echo "no upstream repository exists"
 else
+   # assign the original repository to a remote called "upstream"
+   if git config --list | grep -q $UPSTREAM_REPO; then
+      echo "upstream repo already configured: https://github.com/$UPSTREAM_REPO"
+   else
+      echo
+      read -p "Press enter to assign upstream repository..."
+      if $HTTPS; then
+         git remote add upstream https://github.com/$UPSTREAM_REPO && echo "remote upstream added for https://github.com/$UPSTREAM_REPO"
+      else
+         git remote add upstream git@github.com:$UPSTREAM_REPO && echo "remote upstream added for git@github.com:$UPSTREAM_REPO"
+      fi
+   fi
+
+   # pull in changes not present in local repository, without modifying local files
    echo
-   read -p "Press enter to assign upstream repository..."
-   git remote add upstream https://github.com/$UPSTREAM_REPO && echo "remote upstream added for https://github.com/$UPSTREAM_REPO"
+   read -p "Press enter to fetch changes from upstream repository..."
+   git fetch upstream master
+   echo "upstream fetch done"
+
+   # merge any changes fetched into local working files
+   echo
+   read -p "Press enter to merge changes..."
+   git merge upstream/master
 fi
-
-# pull in changes not present in local repository, without modifying local files
-echo
-read -p "Press enter to fetch changes from upstream repository..."
-git fetch upstream
-echo "upstream fetch done"
-
-# merge any changes fetched into local working files
-echo
-read -p "Press enter to merge changes..."
-git merge upstream/master
 
 # update gems
 echo
