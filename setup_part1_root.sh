96212a35148b386e18b7bf701d44cbb795d9b0ea
Keegan Mullaney <keegan@kmauthorized.com>
2014-08-22 21:41:09 +0800

    rename setup files for root and nonroot parts separate code into proper sections for root or nonroot
    On branch master
    Changes to be committed:
    
    	new file:   setup_part1_root.sh
    	renamed:    setup.sh -> setup_part2_nonroot.sh

---
+++ setup_part1_root.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+echo "*********************************************"
+echo "* A CentOS 7.0 x64 deployment script to      "
+echo "* install dependencies for Middleman and     "
+echo "* Zurb's Foundation 5                        "
+echo "*                                            "
+echo "* Author : Keegan Mullaney                   "
+echo "* Company: KM Authorized LLC                 "
+echo "* Website: http://kmauthorized.com           "
+echo "*                                            "
+echo "* MIT: http://kma.mit-license.org            "
+echo "*                                            "
+echo "* Instructions:                              "
+echo "* - run as root user                         "
+echo "*********************************************"
+
+# check to make sure script is being run as root
+if [ "$(id -u)" != "0" ]; then
+   printf "\033[40m\033[1;31mERROR: root check FAILED (you MUST be root to use this script)! Quitting...\033[0m\n" >&2
+   exit 1
+fi
+
+# set software version here
+EPEL_VERSION='7-0.2'
+RUBY_VERSION='2.1.2'       # to check version - https://www.ruby-lang.org/en/downloads/
+
+# software download URL
+EPEL_URL="http://dl.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-${EPEL_VERSION}.noarch.rpm"
+RUBY_URL="https://get.rvm.io"
+
+# GPG public keys
+EPEL_KEY="http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-$EPEL_VERSION"
+
+########## YUM ##########
+
+# EPEL
+echo
+read -p "Press enter to test the EPEL install..."
+if rpm -qa | grep -q epel
+then
+   echo "EPEL was already configured"
+else
+   read -p "Press enter to import the EPEL gpg key..."
+   # import rpm key
+   ImportPublicKey $EPEL_KEY
+   # list imported gpg keys
+   rpm -qa gpg*
+   # run the install
+   echo
+   read -p "Press enter to continue with EPEL install..."
+   rpm -ivh $EPEL_URL
+   # test new repo
+   echo
+   read -p "Press enter to test the new repo..."
+   yum check-update
+fi
+
+# install Node.js for running the local web server and npm for the CLI
+if rpm -qa | grep -q nodejs; then
+   echo "nodejs was already installed"
+else
+   echo
+   read -p "Press enter to install nodejs and npm..."
+   yum --enablerepo=epel -y install nodejs npm
+fi
+
+# install git
+if rpm -q git; then
+   echo "git was already installed"
+else
+   echo
+   read -p "Press enter to install git..."
+   yum -y install git
+fi
+
+########## GEM ##########
+
+# install Ruby and RubyGems
+read -p "Press enter to install ruby and rubygems..."
+if ruby -v | grep -q "ruby $RUBY_VERSION"; then
+   echo "ruby is already installed"
+else
+   curl -L $RUBY_URL | bash -s stable --ruby=$RUBY_VERSION
+fi
+
+# start using rvm
+echo
+read -p "Press enter to start using rvm..."
+if cat $HOME/.bashrc | grep -q "/usr/local/rvm/scripts/rvm"; then
+   echo "already added rvm to .bashrc"
+else
+   echo "source /usr/local/rvm/scripts/rvm" >> $HOME/.bashrc
+   source /usr/local/rvm/scripts/rvm && echo "rvm sourced and added to .bashrc"
+fi
+
+# update gem package manager
+echo
+read -p "Press enter to update the gem package manager..."
+gem update --system
+
+# install necessary gems
+if $(gem list middleman -i); then
+   echo "gem middleman is already installed"
+else
+   echo
+   read -p "Press enter to install middleman..."
+   gem install middleman
+fi
+
+if $(gem list middleman-blog -i); then
+   echo "gem middleman-blog is already installed"
+else
+   echo
+   read -p "Press enter to install middleman-blog..."
+   gem install middleman-blog
+fi
+
+if $(gem list middleman-syntax -i); then
+   echo "gem middleman-syntax is already installed"
+else
+   echo
+   read -p "Press enter to install middleman-syntax..."
+   gem install middleman-syntax
+fi
+
+if $(gem list middleman-livereload -i); then
+   echo "gem middleman-livereload is already installed"
+else
+   echo
+   read -p "Press enter to install middleman-livereload..."
+   gem install middleman-livereload
+fi
+
+if $(gem list foundation -i); then
+   echo "gem foundation is already installed"
+else
+   echo
+   read -p "Press enter to install foundation..."
+   gem install foundation
+fi
+
+# update gems
+echo
+read -p "Press enter to update gems..."
+gem update
+
+########## NPM ##########
+
+npm install -g bower grunt-cli
+
+ME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "done with $ME"
--- setup_part2_nonroot.sh
+++ setup_part2_nonroot.sh
@@ -10,18 +10,22 @@ echo "* Company: KM Authorized LLC                 "
 echo "* Website: http://kmauthorized.com           "
 echo "*                                            "
 echo "* MIT: http://kma.mit-license.org            "
+echo "*                                            "
+echo "* Instructions:                              "
+echo "* - run as non-root user                     "
 echo "*********************************************"
 
 # check to make sure script is being run as root
 if [ "$(id -u)" != "0" ]; then
-   printf "\033[40m\033[1;31mERROR: Root check FAILED (you MUST be root to use this script)! Quitting...\033[0m\n" >&2
+   echo "non-root user detected, continuing..."
+else
+   printf "\033[40m\033[1;31mERROR: non-root check FAILED (you MUST NOT be root to use this script)! Quitting...\033[0m\n" >&2
    exit 1
 fi
 
 ####################################################
 # EDIT THESE VARIABLES WITH YOUR INFO
 REAL_NAME='Keegan Mullaney'
-USER_NAME='kmullaney' #your Linux non-root user
 EMAIL_ADDRESS='keegan@kmauthorized.com'
 SSH_KEY_COMMENT='my workstation'
 MIDDLEMAN_DOMAIN='keeganmullaney.com'
@@ -32,122 +36,6 @@ GITHUB_USER='keegoid' #your GitHub username
 UPSTREAM_PROJECT='middleman-html5-foundation'
 UPSTREAM_REPO="keegoid/$UPSTREAM_PROJECT.git"
 
-# set software version here
-EPEL_VERSION='7-0.2'
-RUBY_VERSION='2.1.2'       # to check version - https://www.ruby-lang.org/en/downloads/
-
-# software download URL
-EPEL_URL="http://dl.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-${EPEL_VERSION}.noarch.rpm"
-RUBY_URL="https://get.rvm.io"
-
-# GPG public keys
-EPEL_KEY="http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-$EPEL_VERSION"
-
-########## YUM ##########
-# EPEL
-echo
-read -p "Press enter to test the EPEL install..."
-if rpm -qa | grep -q epel
-then
-   echo "EPEL was already configured"
-else
-   read -p "Press enter to import the EPEL gpg key..."
-   # import rpm key
-   ImportPublicKey $EPEL_KEY
-   # list imported gpg keys
-   rpm -qa gpg*
-   # run the install
-   echo
-   read -p "Press enter to continue with EPEL install..."
-   rpm -ivh $EPEL_URL
-   # test new repo
-   echo
-   read -p "Press enter to test the new repo..."
-   yum check-update
-fi
-
-# install Node.js for running the local web server and npm for the CLI
-if rpm -qa | grep -q nodejs; then
-   echo "nodejs was already installed"
-else
-   echo
-   read -p "Press enter to install nodejs and npm..."
-   yum --enablerepo=epel -y install nodejs npm
-fi
-
-# install git
-if rpm -q git; then
-   echo "git was already installed"
-else
-   echo
-   read -p "Press enter to install git..."
-   yum -y install git
-fi
-
-########## GEM ##########
-# install Ruby and RubyGems
-read -p "Press enter to install ruby and rubygems..."
-if ruby -v | grep -q "ruby $RUBY_VERSION"; then
-   echo "ruby is already installed"
-else
-   curl -L $RUBY_URL | bash -s stable --ruby=$RUBY_VERSION
-fi
-
-# start using rvm
-echo
-read -p "Press enter to start using rvm..."
-if cat $HOME/.bashrc | grep -q "/usr/local/rvm/scripts/rvm"; then
-   echo "already added rvm to .bashrc"
-else
-   echo "source /usr/local/rvm/scripts/rvm" >> $HOME/.bashrc
-   source /usr/local/rvm/scripts/rvm && echo "rvm sourced and added to .bashrc"
-fi
-
-# update gem package manager
-echo
-read -p "Press enter to update the gem package manager..."
-gem update --system
-
-# install necessary gems
-if $(gem list middleman -i); then
-   echo "gem middleman is already installed"
-else
-   echo
-   read -p "Press enter to install middleman..."
-   gem install middleman
-fi
-
-if $(gem list middleman-syntax -i); then
-   echo "gem middleman-syntax is already installed"
-else
-   echo
-   read -p "Press enter to install middleman-syntax..."
-   gem install middleman-syntax
-fi
-
-if $(gem list foundation -i); then
-   echo "gem foundation is already installed"
-else
-   echo
-   read -p "Press enter to install foundation..."
-   gem install foundation
-fi
-
-# update gems
-echo
-read -p "Press enter to update gems..."
-gem update
-
-########## NPM ##########
-
-# install bower and grunt-cli
-npm install -g bower grunt-cli
-
-########## NON-ROOT USER ##########
-echo "switching to non-root user..."
-su $USER_NAME
-
-PROJECT_DIRECTORY="$REPOS/$UPSTREAM_PROJECT"
 # local repository location
 REPOS="$HOME/repos"
 if [ -d $HOME/Dropbox ]; then
@@ -277,6 +165,7 @@ else
    middleman init $MIDDLEMAN_DOMAIN --template=blog
    read -p "Press enter to init the foundation-tmp files..."
    foundation new foundation-tmp
+
    # config.rb
    read -p "Press enter to configure middleman-syntax..."
 #   activate :syntax
@@ -342,14 +231,6 @@ else
    git merge upstream/master
 fi
 
-# switch back to root user
-exit
-
-# update gems
-echo
-read -p "Press enter to update gems..."
-gem update
-
 echo
 echo "**********************************************************************"
 echo "* manual steps:                                                       "
@@ -395,4 +276,5 @@ echo '* end                                                                 '
 echo '* ~~~                                                                 '
 echo "**********************************************************************"
 
-echo "done with mm_init.sh"
+ME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+echo "done with $ME"
